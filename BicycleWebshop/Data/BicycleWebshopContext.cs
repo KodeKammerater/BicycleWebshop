9f829d6823132e169f7d54ac2a74d8002ed2045b
Abdi22-7 <ah8808573@gmail.com>
2025-04-08 10:53:56 +0200

    gg prop og scaff

---
+++ BicycleWebshop/Data/BicycleWebshopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BicycleWebshop.Models;
+
+namespace BicycleWebshop.Data
+{
+    public class BicycleWebshopContext : DbContext
+    {
+        public BicycleWebshopContext (DbContextOptions<BicycleWebshopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BicycleWebshop.Models.Order> Order { get; set; } = default!;
+    }
+}
